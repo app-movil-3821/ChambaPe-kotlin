b8b15d20986eda4abb75b3e67622a2ade575b275
Jose Bautista <josediegobautistarivera@gmail.com>
2026-06-10 13:37:15 -0500

    feat(maps): add Google Maps platform dependencies and secrets plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.kotlin.ksp) apply false
-
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
 }
